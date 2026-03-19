380becaa0e693725037e343718867bf18a91c8e3
ardhikaxx <ardhikayanuar58@gmail.com>
2026-03-20 02:39:33 +0700

    Update AppServiceProvider dan tambah vendor views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,5 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+        Paginator::defaultView('vendor.pagination.bootstrap-5-clean');
+        Paginator::defaultSimpleView('vendor.pagination.simple-bootstrap-5-clean');
     }
 }
